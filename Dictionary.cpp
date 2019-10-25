5449160773508128ff5ff6419ad6ace5a9646d13
danielkluzner <35643045+danielkluzner@users.noreply.github.com>
2019-10-25 10:41:31 -0700

    Create Dictionary.cpp

---
+++ Dictionary.cpp
@@ -0,0 +1,25 @@
+#include "Dictionary.h"
+#include <string>
+using namespace std;
+
+
+void Dictionary::bulkInsert(int n, string *keys) {
+// TODO:: Write this.
+
+}
+
+void Dictionary::insert(string key) {
+// TODO:: Write this.
+
+}
+
+void Dictionary::remove(string key) {
+// TODO:: Write this.
+
+}
+
+bool Dictionary::find(string key) {
+// TODO:: Write this.
+
+    return false;
+}
